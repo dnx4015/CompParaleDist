541b0d919f762b5381994b14ec62b009e691f3bd
dnx4015 <dnaranjo@ime.usp.br>
2015-03-26 21:48:37 -0300

    create directory for new types of experiments

---
+++ Code/Directives+CodeChanges/multParallelj.cpp
@@ -0,0 +1,82 @@
+/*
+ID: diana.n1
+PROG: Mult
+LANG: C++
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include "omp.h"
+
+int main(int argc, char **argv) {
+	double start, end; 
+	start = omp_get_wtime();
+    int nthreads, tid, chunk = 10;
+	
+	long **a, **b, **c;
+    int N = 500;
+	
+    if (argc == 2) {
+      N = atoi (argv[1]);
+      assert (N > 0);
+    }
+
+    int i,j,k,mul=5;
+    long col_sum = N * (N-1) / 2;
+	
+	a = (long **)malloc (N * sizeof(long *));
+	b = (long **)malloc (N * sizeof(long *));
+	c = (long **)malloc (N * sizeof(long *));
+	
+	for (i=0; i<N; i++) {
+	  a[i] = (long *)malloc (N * sizeof(long));
+	  b[i] = (long *)malloc (N * sizeof(long));
+	  c[i] = (long *)malloc (N * sizeof(long));
+	}
+
+	#pragma omp parallel shared (a,b,c,nthreads, chunk) private (i, j,k,tid) 
+	{
+		tid = omp_get_thread_num();
+		#pragma omp single
+			printf("Number threads = %d\n", omp_get_num_threads());
+
+		#pragma omp for schedule(static, chunk)
+		for (i=0; i<N; i++){
+			for (j=0; j<N; j++) {
+				a[i][j] = i*mul;
+				b[i][j] = i;
+				c[i][j] = 0;
+	  		}
+		}
+		#pragma omp single
+			printf ("Matrix generation finished.\n");         
+		
+		for (i=0; i<N; i++){
+			#pragma omp for schedule(static, chunk)
+				for (j=0; j<N; j++){
+					for (k=0; k<N; k++){
+						c[i][j] += a[i][k] * b[k][j];
+					}
+		  		}
+		}
+		
+		#pragma omp single
+			printf ("Multiplication finished.\n");         
+	
+		#pragma omp for schedule(static, chunk)
+			for (i=0; i<N; i++){
+			  	for (j=0; j<N; j++){
+					assert ( c[i][j] == i*mul * col_sum);  
+				}
+			}
+		#pragma omp single
+			printf ("Test finished.\n");         
+	}
+
+	end = omp_get_wtime();
+	printf("Time: %lf.\n", end-start);
+}
+
