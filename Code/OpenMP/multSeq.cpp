50c4ed24f3fa744913c9c4590177897f418c5717
dnx4015 <dnaranjo@ime.usp.br>
2015-04-04 19:58:00 -0300

    Essay update conclusions

--- Code/OpenMP/multSeq.cpp
+++ Code/OpenMP/multSeq.cpp
@@ -1,6 +1,6 @@
 /*
 ID: diana.n1
-PROG: Mult
+PROG: MultSeq
 LANG: C++
 */
 
@@ -19,46 +19,45 @@ int main(int argc, char **argv) {
     int N = 500;
 	
     if (argc == 2) {
-      N = atoi (argv[1]);
-      assert (N > 0);
+        N = atoi (argv[1]);
+        assert (N > 0);
     }
 
     int i,j,k,mul=5;
     long col_sum = N * (N-1) / 2;
 	
 			
-		a = (long **)malloc (N * sizeof(long *));
-		b = (long **)malloc (N * sizeof(long *));
-		c = (long **)malloc (N * sizeof(long *));
-		for (i=0; i<N; i++) {
-		  a[i] = (long *)malloc (N * sizeof(long));
-		  b[i] = (long *)malloc (N * sizeof(long));
-		  c[i] = (long *)malloc (N * sizeof(long));
-		}
+	a = (long **)malloc (N * sizeof(long *));
+	b = (long **)malloc (N * sizeof(long *));
+	c = (long **)malloc (N * sizeof(long *));
+	for (i=0; i<N; i++) {
+	    a[i] = (long *)malloc (N * sizeof(long));
+	    b[i] = (long *)malloc (N * sizeof(long));
+	    c[i] = (long *)malloc (N * sizeof(long));
+	}
 
 
-		for (i=0; i<N; i++)
-		  for (j=0; j<N; j++) {
-			a[i][j] = i*mul;
-			b[i][j] = i;
-			c[i][j] = 0;
-		  }
+	for (i=0; i<N; i++)
+	    for (j=0; j<N; j++) {
+		    a[i][j] = i*mul;
+	    	b[i][j] = i;
+		    c[i][j] = 0;
+	    }
 
-		printf ("Matrix generation finished.\n");         
-		
-		for (i=0; i<N; i++)
-		  for (j=0; j<N; j++)
-			for (k=0; k<N; k++)
-				c[i][j] += a[i][k] * b[k][j];
+	printf ("Matrix generation finished.\n");         
+	
+	for (i=0; i<N; i++)
+	    for (j=0; j<N; j++)
+		    for (k=0; k<N; k++)
+		    	c[i][j] += a[i][k] * b[k][j];
 
-		printf ("Multiplication finished.\n");         
-		
-		for (i=0; i<N; i++)
-		  for (j=0; j<N; j++)
-			assert ( c[i][j] == i*mul * col_sum);  
+	printf ("Multiplication finished.\n");         
+	
+	for (i=0; i<N; i++)
+	    for (j=0; j<N; j++)
+		    assert ( c[i][j] == i*mul * col_sum);  
     printf ("Test finished.\n");         
 
 	end = omp_get_wtime();
 	printf("Time: %lf.\n", end-start);
 }
-
